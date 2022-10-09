132d2879d1c306d71f34dc07ee182e84f60178ef
Arnab Biswas <107356840+CoderArnab@users.noreply.github.com>
2022-10-09 14:30:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+#Taking Input from User
+first_Num = input("Enter your First Number :  ")
+second_Num = input("Enter your second Number :   ")
+
+#Calculation
+
+#SUM
+Sum = float(first_Num) + float(second_Num)
+print("Sum is :   " + str(Sum))
+
+#SUBSTRACTION
+Substraction = float(first_Num) - float(second_Num)
+print("Substraction is :   " + str(Substraction))
+
+#MULTIPLICATION
+Multiplication = float(first_Num) * float(second_Num)
+print("Multiplication is :   " + str(Multiplication))
+
+#DIVISION & REMAINDER
+Division = float(first_Num) / float(second_Num)
+Remainder = float(first_Num) % float(second_Num)
+
+print("Division is :  " + str(Division))
+print("Remainder is :  "  + str(Remainder))
+
